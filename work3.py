b9ea573f79662935897b96c1a6a37835760c237d
levsha8d <48731243+levsha8d@users.noreply.github.com>
2023-11-26 13:44:03 +0300

    final

--- work3.py
+++ work3.py
@@ -1,14 +1,14 @@
 class FlatIterator:
     def __init__(self, list_of_lists):
-        self.flattened_list = self.flatten_list(list_of_lists)
+        self.list_of_list = self.flatten_list(list_of_lists)
         self.index = 0
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.index < len(self.flattened_list):
-            item = self.flattened_list[self.index]
+        if self.index < len(self.list_of_list):
+            item = self.list_of_list[self.index]
             self.index += 1
             return item
         else:
